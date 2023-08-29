93b550610b058fdf0daebf9e0303fa1b8313afd7
Jay Sharma <sharmakjay@gmail.com>
2023-08-29 09:34:39 -0600

    Update CalendarEmptyClickEvent.java

--- com/CalendarEmptyClickEvent.java
+++ com/CalendarEmptyClickEvent.java
@@ -4,4 +4,9 @@ import java.awt.*;
 import java.time.LocalDateTime;
 
 public class CalendarEmptyClickEvent extends AWTEvent {
+    private LocalDateTime dateTime;
+
+    public CalendarEmptyClickEvent(Object source, LocalDateTime dateTime) {
+        super(source, 0);
+    }
 }
