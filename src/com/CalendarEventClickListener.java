3fa8d9673760bf30fd2191c7bf8fc957f31170b3
Jay Sharma <sharmakjay@gmail.com>
2023-12-07 12:24:41 -0700

    Create CalendarEventClickListener.java

---
+++ src/com/CalendarEventClickListener.java
@@ -0,0 +1,9 @@
+package com;
+
+import java.util.EventListener;
+
+public interface CalendarEventClickListener extends EventListener {
+    // Event dispatch methods
+    void calendarEventClick(CalendarEventClickEvent e);
+}
+
