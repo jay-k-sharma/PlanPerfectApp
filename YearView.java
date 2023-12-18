12a9df1731e4e8d0edfc00b6360aa3e0ddbcaeac
Jay Sharma <sharmakjay@gmail.com>
2023-12-18 12:25:31 -0700

    Create YearView.java

---
+++ YearView.java
@@ -0,0 +1,14 @@
+import com.*;
+
+// import javax.swing.*;
+// import java.awt.*;
+// import java.time.LocalDate;
+// import java.time.LocalTime;
+// import java.util.ArrayList;
+
+public class YearView {
+    
+    public static void main(String[] args) {
+        new YearCalendar(2023);
+    }
+}
